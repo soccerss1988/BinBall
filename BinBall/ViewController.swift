47ec3fb92a02e33780736325c61f83c9d03ef822
J.Huang <soccerss1988@gmail.com>
2019-09-17 23:50:39 +0800

    Initial Commit

---
+++ BinBall/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BinBall
+//
+//  Created by YJ Huang on 2019/9/17.
+//  Copyright © 2019 YJ Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
